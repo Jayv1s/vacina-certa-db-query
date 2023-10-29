941e52df46b91ca3ade4e929167ee013b8882732
jayv1s <joaovsgcontato@gmail.com>
2023-10-29 13:48:58 -0300

    init commit

---
+++ src/main/java/com/vacinacerta/Main.java
@@ -0,0 +1,18 @@
+package com.vacinacerta;
+
+import com.entities.db.User;
+import com.entities.db.UsersVaccines;
+import com.entities.db.Vaccine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = "com.entities.db", scanBasePackageClasses = Vaccine.class)
+@ComponentScan(basePackages = "com.vacinacerta" )
+@EntityScan(basePackages = "com.entities.db", basePackageClasses = {Vaccine.class, User.class, UsersVaccines.class})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
